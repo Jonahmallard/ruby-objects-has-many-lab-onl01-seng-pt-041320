21b06a11105225a88097ba0e0fabb15b747a7330
Jonah Mallard <benjaminjmallard@gmail.com>
2020-05-10 19:20:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,13 @@
 class Artist 
   attr_accessor :name
- 
+  @songs = []
   def initialize(name)
     @name = name
-     @songs = []  
+      
   end 
   
   def songs
-    @songs 
+    @songs << self.all
   end
   
   def name
